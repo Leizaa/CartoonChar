7dce355ed8176ee4f999a1e7c2af9ef8dd179b89
Leizaa <yudiajalah2@gmail.com>
2021-11-04 15:54:45 +0700

    fix failed to build project, changed targeted sdk to 30

--- app/src/main/java/com/example/cartoonchar/network/CharacterResponse.kt
+++ app/src/main/java/com/example/cartoonchar/network/CharacterResponse.kt
@@ -1,6 +1,13 @@
 package com.example.cartoonchar.network
 
+import com.example.cartoonchar.network.model.Character
+import com.example.cartoonchar.network.model.Info
+import com.google.gson.annotations.SerializedName
+
 data class CharacterResponse(
     val nextPage: Int? = null,
-
+    @SerializedName("info")
+    val info: Info,
+    @SerializedName("results")
+    val data: List<Character>
 )
